3833624ad7a4d4bb17ce3b5f85c1793ba11ded40
Vasja_Pupkin <mainsergeant@mail.ru>
2022-10-18 01:33:07 +0300

    Init commit - First task is completed

---
+++ Seminar1_Homework/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//Задача 2. Напишите программу, которая на входе принимает два числа, и выдает, какое число большее, а какое меньшее.
+
+
+Console.Write("Input first int number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second int number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 < num2){
+    Console.WriteLine($"{num1} < {num2}");
+}
+else {
+    if (num1 == num2){
+        Console.WriteLine($"{num1} = {num2}");
+    }
+    else {
+        Console.WriteLine($"{num1} > {num2}");
+    }
+    
+}
